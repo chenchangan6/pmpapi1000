5801b3e973bf35061a242e665edfa0a9968dcd43
chenchangan6 <chenchangan6@gmail.com>
2018-03-25 16:50:32 +0800

    跟新了测试服务器说明和地址

--- config1000/configs.py
+++ config1000/configs.py
@@ -11,7 +11,7 @@ APP_ENVIRONMENT = {
 # MongoDB 服务器设置。
 # 待完善--后面要加上判断，开发环境和生产环境自动区分。
 MONGODB_CONFIG = {
-    'host': '192.168.31.35',
+    'host': '180.76.121.121',
     'port': 27017,
     'db_name': 'testdb',
     'username': 'pmptikutest',
