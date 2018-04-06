66256f41faae88b2705bc9cb50f94005569a9cf3
chenchangan6 <chenchangan6@gmail.com>
2018-04-06 18:34:20 +0800

    增加main.py文件，删除了run.py

---
+++ main.py
@@ -0,0 +1,23 @@
+#!flask/bin/python3.6
+# -*- coding: utf-8 -*-
+from flask import Flask, Blueprint
+from flask_restful import Resource, Api
+from users1000.users import USERS
+import random
+
+app = Flask(__name__)
+INDEX = Blueprint('index', __name__)
+ApiBlue = Api(INDEX)
+
+
+class HelloWorld(Resource):
+    def get(self):
+        cc = random.randint(0, 100)
+        return {'启动成功': 200, '随机数': cc}
+
+
+ApiBlue.add_resource(HelloWorld, '/')
+app.register_blueprint(INDEX, url_prefix='/')
+app.register_blueprint(USERS, url_prefix='/users/')
+if __name__ == '__main__':
+    app.run(debug=True)
