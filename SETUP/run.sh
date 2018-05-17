fb46e40353db9dc8ababb7e07c3681340c024bb3
chenchangan6 <chenchangan6@gmail.com>
2018-05-18 01:53:33 +0800

    更新

--- SETUP/run.sh
+++ SETUP/run.sh
@@ -4,9 +4,9 @@ service nginx restart
 
 source /var/www/pmpapi1000/flask/bin/activate
 
-gunicorn -D main:app -c /var/www/pmpapi1000/config1000/gunicorn.conf.py
-
-
 echo -e "\e[31;47m**********English: server is running  **********\e[0m"
 echo -e "\e[31;47m**********Chinese: 服务器运行成功！**********\e[0m"
-bash
+
+gunicorn main:app -c /var/www/pmpapi1000/config1000/gunicorn.conf.py
+
+
