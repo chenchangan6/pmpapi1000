e0c2f2f2a7316cfd7f0b3547205374f4b343fb31
chenchangan6 <chenchangan6@gmail.com>
2018-05-18 01:44:23 +0800

    更正拼写错误

--- SETUP/run.sh
+++ SETUP/run.sh
@@ -9,3 +9,4 @@ gunicorn -D main:app -c /var/www/pmpapi1000/config1000/gunicorn.conf.py
 
 echo -e "\e[31;47m**********English: server is running  **********\e[0m"
 echo -e "\e[31;47m**********Chinese: 服务器运行成功！**********\e[0m"
+bash
